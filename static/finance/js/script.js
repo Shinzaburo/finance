b3aef2283739c125f0492ffcdd48c9eeec808b7e
Shinzaburo <zaburo.0316@icloud.com>
2022-02-07 21:39:31 +0900

    commit

--- static/finance/js/script.js
+++ static/finance/js/script.js
@@ -1,38 +1,35 @@
 window.addEventListener("load" , function (){
 
   
+
+  let config_pay_date   = {
+      "locale": "ja"
+  }
+  flatpickr("[name='pay_date']", config_pay_date);
+
+  $('.tab-content>div').hide();
+  $('.tab-content>div').first().slideDown();
+
+  $('.tab-buttons>span').click(function(){
+      var thisclass=$(this).attr('class');
+      $('#lamp').removeClass().addClass('#lamp').addClass(thisclass);
+      $('.tab-content>div').each(function(){
+          if($(this).hasClass(thisclass)){
+              $(this).fadeIn(500);
+          }
+          else{
+              $(this).hide();
+          }
+      });
+
+      $(".tab-buttons > span").css({"background":""});    
+      $(this).css({"background":"silver"});
+
+  });
   
+  //DjangoMessageFrameWorkの削除機能
+  $(".message_delete").on("click", function(){ $(this).parent(".message").remove(); });
 
-    let config_pay_date   = {
-        "locale": "ja"
-    }
-    flatpickr("[name='pay_date']", config_pay_date);
-
-    $(".tab_selected").css({"background":"silver"});
-    
-    $('.tab-content>div').hide();
-    $('.tab-content>div').first().slideDown();
-    $('.tab-buttons>span').click(function(){
-    var thisclass=$(this).attr('class');
-    $('#lamp').removeClass().addClass('#lamp').addClass(thisclass);
-    $('.tab-content>div').each(function(){
-        if($(this).hasClass(thisclass)){
-          $(this).fadeIn(500);
-        }
-        else{
-          $(this).hide();
-        }
-        });
-
-        $(".tab-buttons > span").css({"background":""});
-        $(this).css({"background":"silver"});
-        
-    });
-    
-    
-    //DjangoMessageFrameWorkの削除機能
-    $(".message_delete").on("click", function(){ $(this).parent(".message").remove(); });
-
-    document.getElementById('#free').style.visibility = 'visible';
+  document.getElementById('#free').style.visibility = 'visible';
 
 });
